072ce52279e6aa6fc71cb2a2f4bc5f9240726178
Rafaygithub004 <rafayabdur004@gmail.com>
2023-03-31 11:48:01 -0700

    JS Assignment

---
+++ app.js
@@ -0,0 +1,48 @@
+alert("Ramzan Mubarak")
+
+console.log("Ramzan or JavaScript")
+
+var title = "Ramzan or JavaScript"
+
+console.log(title);
+
+var x = 10;
+var y = x + 20;
+
+console.log(y);
+
+var a = 10;
+var b = 20;
+var c = 30;
+var x = (a + b) - c;
+
+console.log(x);
+
+
+
+var a = 10;
+var b = a++;
+var c = ++a
+var d = --a
+var e = --a
+
+console.log(b)
+console.log(b)
+console.log(a)
+console.log(c)
+console.log(d)
+console.log(e)
+
+var FirstName = prompt("Enter First Name");
+var LastName = prompt("Enter Last Name");
+var City = prompt("Enter City" , "Karachi");
+
+console.log(FirstName + " " + LastName + " From " + City);
+
+var MithaiMilRhiHai = false;
+if (MithaiMilRhiHai == true) {
+    console.log("Ajao");
+} else 
+{
+    console.log("Dusri Masjid");
+}
